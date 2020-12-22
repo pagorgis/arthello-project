e330b6c434bff96cfe3396a214f2e368fed535a8
pgorgis <pgorgis@kth.se>
2020-12-22 15:51:14 +0100

    Test of creating board - not dynamic

---
+++ Assets/Script/CreateSquares.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CreateSquares : MonoBehaviour
+{
+
+    public Transform squareObj;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        for (float x = -0.24f; x < 0.24f; x = x + 0.06f)
+        {
+            for (float z = -0.24f; z < 0.24f; z = z + 0.06f)
+            {
+                Instantiate(squareObj, new Vector3(x, 0.2f, z), squareObj.rotation);
+            }
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
