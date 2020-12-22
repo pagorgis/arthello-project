103eb3958f2363c01a9554a582824eab9941e4dd
pgorgis <pgorgis@kth.se>
2020-12-22 20:02:07 +0100

    Got Instantiate working!! Can create objects on runtime.

--- Assets/Script/AddPiece.cs
+++ Assets/Script/AddPiece.cs
@@ -5,13 +5,14 @@ using UnityEngine;
 public class AddPiece : MonoBehaviour
 {
     public GameObject testObj;
-    public GameObject square;
 
     // Start is called before the first frame update
     void Start()
     {
+        GameObject square = GameObject.Find(gameObject.name);
         Transform tempTransform = square.GetComponent<Transform>();
-        Instantiate(testObj, new Vector3(0f, 0.7f, 0f), Quaternion.identity, tempTransform);
+        GameObject testa = Instantiate(testObj, tempTransform);
+        testa.name = "WORKS";
     }
 
     // Update is called once per frame
@@ -24,7 +25,7 @@ public class AddPiece : MonoBehaviour
 
             /*
             Renderer rend = square.GetComponent<Renderer>();
-
+            //
             //Create a new Material
             Material material = new Material(Shader.Find("Standard"));
             material.color = Color.red;
