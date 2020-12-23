5d757080671ac274f480c233a4a85dd837796ce6
pgorgis <pgorgis@kth.se>
2020-12-23 19:21:46 +0100

    Added piece prefab so combined black/white cylinder

--- Assets/Script/AddPiece.cs
+++ Assets/Script/AddPiece.cs
@@ -13,11 +13,21 @@ public class AddPiece : MonoBehaviour
         GameObject square = GameObject.Find(gameObject.name);
 
         string[] square_num = gameObject.name.Split('_');
-        if (square_num[1] == "44" || square_num[1] == "45" || square_num[1] == "54" || square_num[1] == "55")
+        if (square_num[1] == "44" || square_num[1] == "55")
         {
             Transform tempTransform = square.GetComponent<Transform>();
             GameObject testa = Instantiate(testObj, tempTransform);
             testa.name = "piece_" + square_num[1];
+            placed = true;
+        }
+        else if (square_num[1] == "45" || square_num[1] == "54")
+        {
+            Transform tempTransform = square.GetComponent<Transform>();
+            GameObject testa = Instantiate(testObj, tempTransform);
+            testa.transform.localRotation = Quaternion.Euler(0f, 0f, 180f);
+            testa.transform.localPosition = new Vector3(testa.transform.localPosition.x, 4, testa.transform.localPosition.z); // 4 constant, should be changed
+            testa.name = "piece_" + square_num[1];
+            placed = true;
         }
     }
 
