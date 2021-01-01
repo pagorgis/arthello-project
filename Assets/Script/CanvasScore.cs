9c0b5b059cd63d2916e8a17d74ddb8384d20d4d8
pgorgis <pgorgis@kth.se>
2021-01-01 16:24:41 +0100

    Active color shown in canvas on top

--- Assets/Script/CanvasScore.cs
+++ Assets/Script/CanvasScore.cs
@@ -7,6 +7,8 @@ public class CanvasScore : MonoBehaviour
 {
     public GameObject blackScore;
     public GameObject whiteScore;
+    public GameObject blackTurn;
+    public GameObject whiteTurn;
     // Start is called before the first frame update
     void Start()
     {
@@ -16,11 +18,20 @@ public class CanvasScore : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        //GameObject blackScore = GameObject.Find("Black_score");
-        //GameObject whiteScore = GameObject.Find("White_score");
         Text blackTO = blackScore.GetComponent<Text>();
         Text whiteTO = whiteScore.GetComponent<Text>();
         blackTO.text = "" + OthelloGame.blackScore;
         whiteTO.text = "" + OthelloGame.whiteScore;
+
+        if (OthelloGame.currentTurn == "black")
+        {
+            blackTurn.SetActive(true);
+            whiteTurn.SetActive(false);
+        }
+        else if (OthelloGame.currentTurn == "white")
+        {
+            blackTurn.SetActive(false);
+            whiteTurn.SetActive(true);
+        }
     }
 }
