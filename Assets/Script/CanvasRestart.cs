df4f3f8d2a295a2b307bea8cd9c7b5a01f8f944c
pgorgis <pgorgis@kth.se>
2021-01-02 12:40:36 +0100

    Restart screen pops up if game over

---
+++ Assets/Script/CanvasRestart.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CanvasRestart : MonoBehaviour
+{
+    public GameObject restartScreen;
+    public GameObject restartButton;
+    bool screenShown = false;
+    // Start is called before the first frame update
+    void Start()
+    {
+        restartScreen.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (screenShown == false && OthelloGame.gameOver == true)
+        {
+            restartScreen.SetActive(true);
+            screenShown = true;
+        }
+        else if (screenShown == true && OthelloGame.gameOver == false)
+        {
+            restartScreen.SetActive(false);
+            screenShown = false;
+        }
+    }
+}
